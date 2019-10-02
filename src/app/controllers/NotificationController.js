7c9aa5681cfc198b5c87df7ecb911121bf617a32
Roberto <jrggroberto@gmail.com>
2019-10-01 22:44:52 -0300

    chore: added order desc on notifications

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -17,7 +17,7 @@ class NotificationController {
     const notifications = await Notification.find({
       user: req.userId,
     })
-      .sort('createdAt')
+      .sort({ createdAt: 'desc' })
       .limit(20);
 
     return res.json(notifications);
